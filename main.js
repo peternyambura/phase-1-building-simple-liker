e519c2adce5aa527cc21f4dab531d8542cbac51c
= <peterndirangu0005@gmail.com>
2023-08-01 15:21:33 +0300

    done

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
+// Add an event listener to each like button
+const likeButtons = document.querySelectorAll('.like');
+likeButtons.forEach(likeButton => {
+  likeButton.addEventListener('click', handleLikeClick);
+});
 
-
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
+function handleLikeClick(event) {
+  const heart = event.target.querySelector('.like-glyph');
+  mimicServerCall()
+    .then(() => {
+      if (heart.textContent === EMPTY_HEART) {
+        heart.textContent = FULL_HEART;
+        heart.classList.add('activated-heart');
       } else {
-        resolve("Pretend remote server notified of action!");
+        heart.textContent = EMPTY_HEART;
+        heart.classList.remove('activated-heart');
       }
-    }, 300);
-  });
+    })
+    .catch((error) => {
+      const modal = document.getElementById('modal');
+      const modalMessage = document.getElementById('modal-message');
+      modal.classList.remove('hidden');
+      modalMessage.textContent = error;
+      setTimeout(() => {
+        modal.classList.add('hidden');
+      }, 3000);
+    });
 }
